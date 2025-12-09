1ff00203ef0cf5c82f921c4a708fab04c5bc458c
majidproject <mm.project.8902@gmail.com>
2025-12-10 02:03:05 +0330

    Updates

--- _next/static/O-E5qLO7giptApGFriZkz/_buildManifest.js
+++ _next/static/O-E5qLO7giptApGFriZkz/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/client-pitch-demo//_next/:path+",
-        "destination": "/client-pitch-demo/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
