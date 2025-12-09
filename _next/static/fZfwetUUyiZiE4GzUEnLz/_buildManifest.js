7777af459984a08e50aca2a38867a4b928ba30fc
majidproject <mm.project.8902@gmail.com>
2025-12-10 01:48:21 +0330

    Updates

--- _next/static/fZfwetUUyiZiE4GzUEnLz/_buildManifest.js
+++ _next/static/fZfwetUUyiZiE4GzUEnLz/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/client-pitch-demo//_next/:path+",
+        "destination": "/client-pitch-demo/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
